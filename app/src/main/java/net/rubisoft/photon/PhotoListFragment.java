6ad6e2fd4c082f15939e0b01b9b545636e778222
Yavor <yavor87@gmail.com>
2016-06-05 16:08:21 +0300

    Adding 5.0 style permission checking

--- app/src/main/java/net/rubisoft/photon/PhotoListFragment.java
+++ app/src/main/java/net/rubisoft/photon/PhotoListFragment.java
@@ -1,8 +1,12 @@
 package net.rubisoft.photon;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,21 +27,45 @@ public class PhotoListFragment extends Fragment {
     }
 
     private ImageProvider mProvider;
+    private GridView mGridView;
+    private static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 101;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mProvider = new net.rubisoft.photon.data.LocalImageProvider(getContext());
-//        mProvider = new net.rubisoft.photon.data.SampleImageProvider();
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            mProvider = new net.rubisoft.photon.data.LocalImageProvider(getContext());
+        } else {
+            requestPermissions(new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                    READ_EXTERNAL_STORAGE_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == READ_EXTERNAL_STORAGE_PERMISSION_REQUEST && grantResults.length == 1 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            mProvider = new net.rubisoft.photon.data.LocalImageProvider(getContext());
+        } else {
+            mProvider = new net.rubisoft.photon.data.SampleImageProvider();
+        }
+        setAdapter();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_list, container, false);
-        GridView gv = (GridView) view.findViewById(R.id.list);
-        gv.setAdapter(new SampleGridViewAdapter(this.getContext(), mProvider.getImages()));
+        mGridView = (GridView) view.findViewById(R.id.list);
+        if (mProvider != null) {
+            setAdapter();
+        }
         return view;
+    }
+
+    private void setAdapter() {
+        mGridView.setAdapter(new SampleGridViewAdapter(this.getContext(), mProvider.getImages()));
     }
 }
