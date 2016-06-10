133a1fe27c6667fb02fb6536842c66cdb3e4ba84
Yavor <yavor87@gmail.com>
2016-06-10 13:46:17 +0300

    Replaced listview with linearlayout in details

--- app/src/main/java/net/rubisoft/photon/ImageFragment.java
+++ app/src/main/java/net/rubisoft/photon/ImageFragment.java
@@ -12,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 
 import com.squareup.picasso.Picasso;
 
@@ -34,7 +34,7 @@ public class ImageFragment extends Fragment implements LoaderManager.LoaderCallb
 
     private int mImageID;
     private ImageView mImageView;
-    private ListView mCategoriesView;
+    private LinearLayout mCategoriesView;
     private ImageCategoriesCursorAdapter mAdapter;
 
     public ImageFragment() {
@@ -77,10 +77,9 @@ public class ImageFragment extends Fragment implements LoaderManager.LoaderCallb
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_image, container, false);
         mImageView = (ImageView) view.findViewById(R.id.detail_image);
-        mCategoriesView = (ListView) view.findViewById(R.id.detail_category_list);
+        mCategoriesView = (LinearLayout) view.findViewById(R.id.detail_category_list);
 
         mAdapter = new ImageCategoriesCursorAdapter(getContext(), null, 0);
-        mCategoriesView.setAdapter(mAdapter);
 
         return view;
     }
@@ -108,6 +107,7 @@ public class ImageFragment extends Fragment implements LoaderManager.LoaderCallb
             }
         } else {
             mAdapter.swapCursor(data);
+            populateCategories();
         }
     }
 
@@ -117,6 +117,17 @@ public class ImageFragment extends Fragment implements LoaderManager.LoaderCallb
             mImageView.setImageURI(null);
         } else {
             mAdapter.swapCursor(null);
+            populateCategories();
+        }
+    }
+
+    private void populateCategories() {
+        mCategoriesView.removeAllViews();
+        final int adapterCount = mAdapter.getCount();
+
+        for (int i = 0; i < adapterCount; i++) {
+            View item = mAdapter.getView(i, null, null);
+            mCategoriesView.addView(item);
         }
     }
 }
